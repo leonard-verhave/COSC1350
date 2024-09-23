fbce332d559e675722a14d7e8b0346c2447e2d0e
leonard-verhave <leonard.verhave@icloud.com>
2024-09-23 13:56:41 -0600

    Add files via upload

---
+++ variablesPractice.js
@@ -0,0 +1,20 @@
+/* 
+Name: leonard verhave
+Filename: practiceVariables 
+date: 9-23-24
+*/
+
+let hiDrT = "hi Dr. T";
+let piValue = 4.5;
+let zeroValue = 0;
+let negativeValue = -10;
+let itsTrue = true;
+
+console.log("hi Dr.T !\nLet's get started!");
+console.log(`half of 100 is ${100 / 2}`);
+console.log("con" + "cat" + "e" + "nate");
+console.log( 8 * null);
+console.log("5"- 1);
+console.log("5"+ 1);
+console.log("five" * 2);
+console.log(false == true);
