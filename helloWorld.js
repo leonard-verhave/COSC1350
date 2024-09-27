303374bbce6f66cb3ed90dbdf1017d58f736872c
leonard-verhave <leonard.verhave@icloud.com>
2024-09-27 13:45:10 -0600

    Add files via upload

---
+++ helloWorld.js
@@ -0,0 +1,7 @@
+/* 
+Name: leonard verhave
+Filename: helloWorld.js 
+Date: 9-27-24
+*/
+let messege = "Hello World";
+console.log(messege);
---
+++ practiceVariables.js
@@ -0,0 +1,26 @@
+/* 
+Name: leonard verhave
+Filename: practiceVariables.js 
+Date: 9-27-24
+*/
+
+let hiDrT = "Hi Dr. T!";
+let number1 = 4.5;
+let number2 = 0;
+let number3 = -10;
+let isTrue = true;
+
+console.log(typeof hiDrT);
+console.log(typeof number1);
+console.log(typeof number2);
+console.log(typeof number3);
+console.log(typeof isTrue);
+
+console.log("Hi Dr. T!\nLet's get started!");
+console.log(`half of 100 is ${100 / 2}`);
+console.log("con" + "cat" + "e" + "nate");
+console.log(8 * null);
+console.log("5" - 1);
+console.log("5" + 1);
+console.log("five" * 2);
+console.log(false == true);
